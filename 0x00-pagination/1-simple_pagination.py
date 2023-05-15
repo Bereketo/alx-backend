761c6e6a8740543353f5133f38944f8d9bfd56bd
Berekto <bereketalebachew87@gmail.com>
2023-05-15 23:36:11 +0300

    fix on simple paginatio

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,8 +7,6 @@ from typing import List, Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """ return a tuple of size two containing a start index and an end index
-        corresponding to the range of indexes to return in a
-        list for those particular pagination parameters.
     """
     startIndex = (page - 1) * page_size
     endIndex = startIndex + page_size
