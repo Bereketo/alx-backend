3ec4a0cb041b509f118fb445e2dad5b585208920
Berekto <bereketalebachew87@gmail.com>
2023-05-16 00:07:05 +0300

    page info

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -66,10 +66,8 @@ class Server:
         return hyper
 
 
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ return a tuple of size two containing a start index and an end index
-        corresponding to the range of indexes to return in a
-        list for those particular pagination parameters.
     """
     startIndex = (page - 1) * page_size
     endIndex = startIndex + page_size
