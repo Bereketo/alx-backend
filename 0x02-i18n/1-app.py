edeed5b2a4bc1d8c21527ffe8e091219ccd36330
Berekto <bereketalebachew87@gmail.com>
2023-05-23 21:03:52 +0300

    babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from flask_babel import Babel
+from flask import Flask, render_template
+import pytz
+
+app = Flask(__name__)
+bable = Babel(app)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
+app.config['TIMEZONE'] = 'UTC'
+
+
+@app.route('/')
+def index():
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
