618a1036ed03bb196b4250fd8c7b445a1ab65fd3
Berekto <bereketalebachew87@gmail.com>
2023-05-24 16:16:16 +0300

    localize with url bug fix

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,6 +19,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """determines best match language
     """
@@ -29,7 +30,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
